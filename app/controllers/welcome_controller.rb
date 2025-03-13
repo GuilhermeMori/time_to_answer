ae038a0d06515e8ef3e110883ea1bb8579a2cd9a
GuiluilhermeMori <guilhermebmori@hotmail.com>
2025-03-13 10:14:30 -0300

    create views profiles and admins

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,5 @@
 class WelcomeController < ApplicationController
-  http_basic_authenticate_with name: "guilherme", password: "12345"
-
+  # http_basic_authenticate_with name: "guilherme", password: "12345"
   def index
   end
 end
